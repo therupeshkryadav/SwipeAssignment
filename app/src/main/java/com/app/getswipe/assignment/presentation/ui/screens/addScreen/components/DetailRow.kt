f4beca0a7ab341256d900298eef38bc0846c4b1f
Rupesh <rupesh.official484@gmail.com>
2025-02-01 15:58:13 +0530

    completed!!

--- app/src/main/java/com/app/getswipe/assignment/presentation/ui/screens/addScreen/components/DetailRow.kt
+++ app/src/main/java/com/app/getswipe/assignment/presentation/ui/screens/addScreen/components/DetailRow.kt
@@ -1,8 +1,6 @@
 package com.app.getswipe.assignment.presentation.ui.screens.addScreen.components
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,10 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
