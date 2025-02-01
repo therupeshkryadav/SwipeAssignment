68fddd20150c9110b1a6d141e1e514cc73c8afaa
Rupesh <rupesh.official484@gmail.com>
2025-02-01 16:24:31 +0530

    completed!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.12"
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
